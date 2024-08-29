8dbc8d13fa8bc99cad3e7f71172e611d6972fba5
Wynn <wynnforthework@gmail.com>
2024-08-29 15:36:03 +0800

    Update 同步书签.bat

--- 同步书签.bat
+++ 同步书签.bat
@@ -1,6 +1,28 @@
 @echo off
-set source=C:\Users\Administrator\AppData\Local\google\Chrome\User Data\Default\Bookmarks
-set destination=D:\Unity\Projects\pintree\json
-set newname=pintree.json
- 
-copy "%source%" "%destination%\%newname%"
+setlocal enabledelayedexpansion
+
+:: 获取当前用户名
+for /F "tokens=*" %%i in ('whoami') do set "username=%%i"
+
+:: 获取AppData目录路径
+set "appdataPath=C:\Users\%username%\AppData\Local\google\Chrome\User Data\Default\"
+
+:: 输出选项
+echo 请选择一个选项：
+echo 1. 将文件 %appdataPath%\Bookmarks 复制到当前目录并命名为 pintree.json
+echo 2. 将当前目录的 .\json\pintree.json 文件覆盖 %appdataPath%\Bookmarks 文件
+
+set /p choice=请输入选项编号（1或2）：
+
+if "%choice%"=="1" (
+    copy "%appdataPath%\Bookmarks" "%cd%\json\pintree.json"
+    echo 文件已复制并命名为 pintree.json
+) else if "%choice%"=="2" (
+    copy "%cd%\json\pintree.json" "%appdataPath%\Bookmarks"
+    echo 文件已覆盖 %appdataPath%\Bookmarks
+) else (
+    echo 无效的选项
+)
+
+endlocal
+pause

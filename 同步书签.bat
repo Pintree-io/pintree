85232a7d6ff7412affb3b45ca584b8b6e7975070
Wynn <wynnforthework@gmail.com>
2024-08-29 16:20:02 +0800

    Update 同步书签.bat

--- 同步书签.bat
+++ 同步书签.bat
@@ -1,25 +1,30 @@
 @echo off
+chcp 65001 >nul
 setlocal enabledelayedexpansion
 
 :: 获取当前用户名
-for /F "tokens=*" %%i in ('whoami') do set "username=%%i"
+echo.
+echo 当前用户:%USERNAME%
 
 :: 获取AppData目录路径
-set "appdataPath=C:\Users\%username%\AppData\Local\google\Chrome\User Data\Default\"
+set targetPath=C:\Users\%USERNAME%\AppData\Local\google\Chrome\User Data\Default\Bookmarks
+set sourcePath=%cd%\json\pintree.json
 
 :: 输出选项
 echo 请选择一个选项：
-echo 1. 将文件 %appdataPath%\Bookmarks 复制到当前目录并命名为 pintree.json
-echo 2. 将当前目录的 .\json\pintree.json 文件覆盖 %appdataPath%\Bookmarks 文件
+echo.
+echo "1 %targetPath% 覆盖 %sourcePath%"
+echo.
+echo "2 %sourcePath% 覆盖 %targetPath%"
 
 set /p choice=请输入选项编号（1或2）：
 
 if "%choice%"=="1" (
-    copy "%appdataPath%\Bookmarks" "%cd%\json\pintree.json"
-    echo 文件已复制并命名为 pintree.json
+    copy "%targetPath%" "%sourcePath%"
+    echo 文件已复制 %sourcePath%
 ) else if "%choice%"=="2" (
-    copy "%cd%\json\pintree.json" "%appdataPath%\Bookmarks"
-    echo 文件已覆盖 %appdataPath%\Bookmarks
+    copy "%sourcePath%" "%targetPath%"
+    echo 文件已覆盖 %appdataPath%
 ) else (
     echo 无效的选项
 )
